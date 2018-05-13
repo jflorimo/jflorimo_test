5b01e2baf3360979de97737d09221e2626a4dfa2
Jean Florimont <flojeanc@yahoo.fr>
2018-05-13 22:53:46 +0200

    rafactor()

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 nohup python /data/web/manage.py process_tasks --log-std &
 gunicorn app.wsgi:application -w 2 -b :80
